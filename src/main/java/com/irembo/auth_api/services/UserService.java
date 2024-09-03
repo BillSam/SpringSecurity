aba890ffa5ce883ad0896be0b8f34cbdf91251f8
Samuel Nganga <samuelnganga@Irembo-095-SNganga.local>
2024-09-03 14:39:22 +0200

    added the user endpoints

---
+++ src/main/java/com/irembo/auth_api/services/UserService.java
@@ -0,0 +1,23 @@
+package com.irembo.auth_api.services;
+
+import com.irembo.auth_api.entities.User;
+import com.irembo.auth_api.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
